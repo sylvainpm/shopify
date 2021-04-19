12a6413dcb6ef547d75e3486bbb5120bff84f881
sylvainpm <you@example.com>
2021-04-19 12:53:42 +0000

    procfile

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\Produit;
 use Illuminate\Database\Seeder;
+use Database\Seeders\RoleSeeder;
 use Database\Seeders\ProduitSeeder;
+use Database\Seeders\CategorySeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,11 +19,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        $this->call([ProduitSeeder::class]);
+        $this->call([
+
+
+            RoleSeeder::class,
+
+          // ProduitSeeder::class ,
+         //   CategorySeeder::class,
+
+        ]);
+
+
+
 
 
        //o peut commenter ça maintenent         Produit::factory(50)->create() ;
-        // si on fait use App\Models\Produit comme la ligne 5 vaut mieux garder la ligne 28;   \App\Models\Produit::factory(50)->create() ;
+        // si on fait use App\Models\Produit comme la ligne 5 vaut mieux garder la ligne 28;
+         \App\Models\Produit::factory(50)->create() ;
         \App\Models\User::factory(10)->create();
 
 
